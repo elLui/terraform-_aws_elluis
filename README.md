611d50fe4e0af0be9dfb6dc18ade7392db73797c
elluis.codes <elluis.codes@gmail.com>
2021-05-16 17:35:49 -0500

    Initial commit -
    	rebuilding terraform deployment application

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # terraform-_aws_elluis
 AWS deployment solution - Personal VPC - back end state file will only be kept on local machine
+
+
+
+STAGING BUILD ONLY
---
+++ _1_main.tf
@@ -0,0 +1,19 @@
+# portfolio project // blog infrastructure
+# @author luis_cortez elluis.codes@gmail.com
+# @version 1.00
+
+
+# config aws provicer and region template
+
+terraform {
+  required_providers {
+      aws = {
+          source = "hashicorp/aws"
+          versversion = "~> 3.0"
+      }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
